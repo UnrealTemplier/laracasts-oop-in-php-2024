0421ea3f2f3862b3a2dc158a233d09ab286d86bb
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-11 19:45:30 +0400

    Add Lesson 02 - Objects.

---
+++ 02. Objects/index.php
@@ -0,0 +1,23 @@
+<?php
+
+class Playlist
+{
+    public function __construct(public $name, public $songs) {}
+
+    public function shuffle()
+    {
+        shuffle($this->songs);
+    }
+}
+
+$playlists = [];
+
+$playlists[] = new Playlist('90s Depression', [
+    'My Heart Will Go On',
+    'It\'s My Life',
+    'I\'m a Scatman',
+]);
+
+$playlists[0]->shuffle();
+
+print_r($playlists);
