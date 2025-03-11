e1d23e079663eb56345534bfd534e1e5af8f713b
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-11 16:35:36 +0400

    Add Lesson 01 - Classes.

---
+++ 01. Classes/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Examples of classes
+
+class Person
+{
+    public function talk()
+    {
+        //
+    }
+
+    public function walk()
+    {
+        //
+    }
+}
+
+class Category
+{
+    public function add()
+    {
+        //
+    }
+
+    public function remove()
+    {
+        //
+    }
+}
+
+class Post
+{
+    public function edit()
+    {
+        //
+    }
+}
